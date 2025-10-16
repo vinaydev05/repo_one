e22c9256c74bba1ec2ed20904d8dd0b7e576c679
vinaydev05 <vinaydev05@gmail.com>
2025-10-16 19:29:53 +0530

    16/10/2025

---
+++ TestScripts/git_prct_one.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SeleniumLearning.TestScripts
+{
+    public class git_prct_one
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void StartBrowser()
+        {
+            new WebDriverManager.DriverManager().SetUpDriver(new FirefoxConfig());
+            driver = new FirefoxDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.Url = "";
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+           Thread.Sleep(1000);
+            driver.Dispose();
+        }
+       
+       
+    }
+}
